1eb4a7083491bd232e7dcb609b915d65f5efe917
Alex Efremov <alex.efremov.dev@gmail.com>
2017-01-21 21:35:47 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <h1>
+      Full list of competitions:
+    </h1>
+    <ul>
+      <li v-for="league in leagues">
+        {{ league.caption }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async data (context) {
+    const { data } = await axios.get('http://api.football-data.org/v1/competitions')
+    return { leagues: data }
+  }
+}
+</script>
+
+<style scoped>
+</style>
