99d5eff6076b1a4aee98bb86b1b31ac047370c90
Alex Efremov <alex.efremov.dev@gmail.com>
2017-01-21 23:48:52 +0200

    Add league page

---
+++ pages/leagues/_league.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h3>{{ leagueTable.leagueCaption }}</h3>
+    <h5>{{ leagueTable.matchday }}</h5>
+    <table>
+      <tbody>
+        <tr v-for="team in leagueTable.standing">
+          <td>{{ team.position }}</td>
+          <td>{{ team.teamName }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+const leagueIds = {
+  'premier': 426,
+  'championship': 427,
+  'bundesliga': 430,
+  'bundesliga-2': 431,
+  'eredivisie': 433,
+  'league-1': 434,
+  'league-2': 435,
+  'primera': 436,
+  'segunda': 437,
+  'serie-a': 438,
+  'primeira': 439,
+};
+import axios from 'axios'
+const leagueIndexes = ['']
+export default {
+  name: 'league',
+  async data (context) {
+    const id = leagueIds[context.params.league];
+    const { data } = await axios.get(`http://api.football-data.org/v1/competitions/${id}/leagueTable`)
+    return { leagueTable: data }
+  }
+}
+</script>
+
+<style scoped>
+</style>
